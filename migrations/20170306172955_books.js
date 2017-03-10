ab62dce4f2dbc3d08c931c035b4692622d216ca2
Mary Lai <marylychee@gmail.com>
2017-03-10 11:17:04 -0800

    complete migrations and seeding, begin routes

--- migrations/20170306172955_books.js
+++ migrations/20170306172955_books.js
@@ -1,15 +1,11 @@
+'use strict';
 
 exports.up = function(knex) {
   return knex.schema.createTable('books', (table) => {
-    // integer id
     table.increments();
-    // title
     table.string('title').notNullable().defaultTo('');
-    // author
     table.string('author').notNullable().defaultTo('');
-    // genre
     table.string('genre').notNullable().defaultTo('');
-    // description
     table.text('description').notNullable().defaultTo('');
     table.text('cover_url').notNullable().defaultTo('');
     table.timestamps(true, true);
--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,19 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+var knex = require('../db/knex');
+
 // YOUR CODE HERE
+router.get('/english', function(req, res) {
+  res.send('Hello world');
+});
+
+router.get('/spanish', function(req, res) {
+  res.send('Hola mundo');
+});
+
+
+
+
 
 module.exports = router;
