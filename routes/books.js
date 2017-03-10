c338a6fbbd8e6030413590c364d2e4e14f0f637d
Mary Lai <marylychee@gmail.com>
2017-03-10 15:22:51 -0800

    Route /books

--- routes/books.js
+++ routes/books.js
@@ -8,14 +8,100 @@ const router = express.Router();
 var knex = require('../db/knex');
 
 // YOUR CODE HERE
-router.get('/english', function(req, res) {
-  res.send('Hello world');
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.get('/spanish', function(req, res) {
-  res.send('Hola mundo');
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
+router.post('/books', (req, res, next) => {
+  knex('books')
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+    }, '*')
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('books')
+        .update({
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+        }, '*')
+        .where('id', req.params.id);
+    })
+    .then((books) => {
+      res.send(books[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  let book;
+
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if (!row) {
+        return next();
+      }
+
+      artist = row;
+
+      return knex('books')
+        .del()
+        .where('id', req.params.id);
+    })
+    .then(() => {
+      delete book.id;
+      res.send(book);
+    });
+    .catch((err) => {
+      next(err);
+    });
+});
 
 
 
