cc80d86c6e91ee23db5a3cb8076ddc61f09f47e8
Mary Lai <marylychee@gmail.com>
2017-03-12 22:02:22 -0700

    complete GET POST DELETE, still need to fix POST with bad email

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,68 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../db/knex');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
-// YOUR CODE HERE
+const {
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps');
+
+
+
+router.get('/token', (req, res, next) => {
+    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+        if (err) {
+            //unauthorized
+            res.set('Content-type', 'application/json');
+            res.status(200).send(false);
+        } else {
+            res.set('Content-type', 'application/json');
+            res.status(200).send('true');
+        }
+    });
+})
+
+router.post('/token', (req, res, next) => {
+    knex('users')
+        .where('email', req.body.email)
+        .then((res_row) => {
+            bcrypt.compare(req.body.password, res_row[0].hashed_password)
+              .then(function(res) {
+                // res can be true or false
+                return res;
+              })
+              .then(function(true_res) {
+                // res is true and our passwords are matched
+                const claim = {
+                    userId: res_row[0].id
+                };
+                const token = jwt.sign(claim, process.env.JWT_KEY, {
+                    expiresIn: '7 days'
+                });
+
+                res.cookie('token', token, {
+                    path: '/',
+                    httpOnly: true,
+                    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 days)
+                    secure: router.get('env') === 'production' // Set from the NODE_ENV
+                });
+
+                delete res_row[0].hashed_password;
+
+                res.send(camelizeKeys(res_row[0]));
+
+              })
+        });
+});
+
+router.delete('/token', (req, res, next) => {
+  res.clearCookie('token', { path: '/token' });
+  res.send(true);
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-var knex = require('../db/knex');
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex');
+
 
 const {
    camelizeKeys,
@@ -10,10 +12,24 @@ const {
 } = require('humps');
 
 router.post('/users', (req, res, next) => {
-  knex('users')
-    .then((users) => {
-        res.send(users);
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      return knex('users')
+        .insert({
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          hashed_password: hashed_password
+        }, '*');
     })
+    .then((users) => {
+      const user = users[0];
+      delete user.hashed_password;
+      res.send(camelizeKeys(user));
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
